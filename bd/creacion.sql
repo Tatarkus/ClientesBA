9adc958da7197edd4b5b2c84b3b6f543eacac032
ThomasFarias <42778501+ThomasFarias@users.noreply.github.com>
2019-06-24 18:28:45 -0400

    registros de manteción

--- bd/creacion.sql
+++ bd/creacion.sql
@@ -13,3 +13,14 @@ id_usuario int not null,
 descripcion varchar(30) not null,
 fechayhora datetime,
 foreign key (id_usuario) references usuario(id));
+
+
+insert into mantencion (id_usuario, descripcion, fechayhora) 
+  values (0,"Necesito un mantenimiento debido a que mi equipo X tiene un problema con la valvula de expansión.",DateTime("now"));
+insert into mantencion (id_usuario, descripcion, fechayhora) 
+  values (0,"El problema con la valvula de expansión persiste.",DateTime("now"));
+
+  insert into mantencion (id_usuario, descripcion, fechayhora) 
+  values (1,"Necesito un mantenimiento debido a que mi equipo X se le ha quemado el termostato.",DateTime("now"));
+insert into mantencion (id_usuario, descripcion, fechayhora) 
+  values (1,"Necesito una mantención urgente, el gas refrigerante de mi aire acondicionado se acabo.",DateTime("now"));
